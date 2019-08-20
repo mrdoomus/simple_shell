c06c95aeabb03d2d8de8503f2620ba4f3429625a
jalondono <juanlondono151776@hotmail.com>
2019-08-20 15:17:19 +0000

    Features: upload header shell.h and function _splitline to token a string

---
+++ _splitline.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+/**
+ * createArgs - split a char * into char **
+ * @line_buf: pointer to be splited
+ * Description: split a pointer to char
+ * Return: double pointer with split pointer
+ * for Holberton project
+ */
+
+char **_splitline(char *line_buf)
+{
+       char **args;
+       int i = 0;
+       char *token;
+
+/*      Do malloc to allocate the necesary memory to char**  */
+	args = (char **)malloc(sizeof(char) * 1024);
+/*      If malloc cant allocate memory just return NULL   */
+	if (args == NULL)
+		return (NULL);
+/*      Do if the arguments is not NULL */
+	if (line_buf != NULL)
+		return (NULL);
+               {
+                       token = strtok(line_buf, " ");
+                       while (token != NULL)
+                       {
+                               args[i] = token;
+			       i++;
+                               token = strtok(NULL, " ");
+                       }
+                       args[i] = NULL;
+               }
+
+       return (args);
+}
--- shell.h
+++ shell.h
@@ -1,3 +1,11 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+char **_splitline(char *line_buf);
+char *_getline(void);
 #endif
