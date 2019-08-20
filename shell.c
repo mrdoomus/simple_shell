e2ae104dca8827fadd32a138f5617c193f417f53
jalondono <juanlondono151776@hotmail.com>
2019-08-20 18:12:18 +0000

    Feature: implement loop to printf Args content

--- shell.c
+++ shell.c
@@ -5,12 +5,19 @@ int main(void)
 	char *line;
 	char **args;
 	int status;
+	int i = 0;
 
 	do {
+		i = 0;
 		write(STDOUT_FILENO, "$ ", 2);
 		line = _getline();
 		args = _splitline(line);
 		status = _execute(args);
+		while (args[i])
+		{
+			printf("%s\n", args[i]);
+			i++;
+		}
 
 		free(line);
 		free(args);
