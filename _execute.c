811ec6a6aec7a61a6550fa1f388a89b283d1c4be
mrdoomus <mrdoom.official@gmail.com>
2019-08-20 15:56:28 +0000

    Features: Changed name of execve to _execute and modified code

---
+++ _execute.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * main - execve example
+ *
+ * Return: Always 0.
+ */
+int execute(char **args)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+
+	if (pid == 0)
+	{
+
+		if (execve(args[0], args) == -1)
+			perror("ERROR: Couldn't execute program");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+		perror("ERROR: Child couldn't be created");
+	else
+		wait(&status);
+	
+	return (1);
+}
