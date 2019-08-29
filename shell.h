a3d93cd84330909f3081079f18550c1422e96782
mrdoomus <mrdoom.official@gmail.com>
2019-08-29 03:39:48 +0000

    Master: Re-organized header with comments of prototypes

--- shell.h
+++ shell.h
@@ -8,24 +8,27 @@
 #include <string.h>
 #include <fcntl.h>
 #include <signal.h>
+/*Functions*/
 extern char **environ;
-char **_splitline(char *line_buf);
 char *_getline(void);
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+char **_splitline(char *line_buf);
 int _execute(char **args, int *flag, int mode);
 char *_pathfinder(char *command, int mode);
 char *_catpath(char **uncatpath, char *command);
-char *_strcat(char *dest, char *src);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
 int _builtins(char **args);
 void _cd(char **args);
 void _env(void);
+/*Utilities*/
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 int _strcmp_path(char *s1, char *s2);
-void _nonintmode(char *line, char **args);
 int _strcspn(char *s, const char *delim);
 int _strspn(char *s, const char *delim);
+void _nonintmode(char *line, char **args);
+/*Proto-Functions*/
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 char *_strtok_r(char *s, const char *delim, char **save_str);
 char *_strtok(char *s, const char *delim);
 #endif
